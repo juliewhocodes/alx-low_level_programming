8eca963bb63fa14a950159dc2551c3b439b0d131
juliewhocodes <juliewanja6@gmail.com>
2023-01-01 10:18:53 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
+
 /**
- * main- program that prints all possible different combinations
- * of thress digits.
- *
- * Return: 0
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-	int n;
-	int j;
+	int n, m, l;
 
-	for(i = 48; i < 58; i++)
+	for (n = 48; n < 58; n++)
 	{
-		for (n = 48; n < 58; n++)
+		for (m = 49; m < 58; m++)
 		{
-			if (n > i)
-			{
-				for (j = 48; j < 58; j++)
+			for (l = 50; l < 58; l++)
+				if (l > m && m > n)
 				{
-					if (j > n)
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
 					{
-						putchar(i);
-						putchar(n);
-						putchar(j);
-						if (i == 55 && n == 56 && j == 57)
-						{
-							break;
-						}
-
 						putchar(',');
 						putchar(' ');
 					}
 				}
-			}
 		}
 	}
-
-	putchar('\n');
-	return (0);
 }
+putchar('\n');
+return (0);
+}
+
 
