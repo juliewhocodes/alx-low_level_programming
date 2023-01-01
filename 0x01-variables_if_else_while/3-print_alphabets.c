cf7c536ee1fea8134dec77b3604bd0d061b25417
juliewhocodes <juliewanja6@gmail.com>
2023-01-01 04:57:50 -0800

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - returns alphabets both lower and upper cases
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char c;
+	int ch = 'a';
+	int CH = 'A';
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	for (c = 'A'; C <= 'z'; c++)
-		putchar(c);
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <='Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
 	putchar('\n');
 	return (0);
 }
-
